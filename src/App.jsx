edf31d9a70bc919684b5aeff2d43dc44b026c056
Jinseong Jeon <aimer120@nate.com>
2026-02-05 17:44:18 +0900

    feat: add previous todos load function

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,42 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import Todos from './components/Todos';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import TodoCreator from './components/TodoCreator';
 import TodoFilter, { FilterTypes } from './components/TodoFilter';
 
 library.add(fas, far, fab);
 let id = 0;
 
+const TodoAppKeys = Object.freeze({
+  filter: 'filter',
+  todos: 'todos',
+});
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   const [selecteFilter, setFilter] = useState(FilterTypes.all);
+  const [todos, setTodos] = useState([]);
   const filteredTodos = useMemo(
     () => filterTodos(todos, selecteFilter),
     [selecteFilter, todos],
   );
+
+  useEffect(() => {
+    setFilter(localStorage.getItem(TodoAppKeys.filter) ?? FilterTypes.all);
+    setTodos(JSON.parse(localStorage.getItem(TodoAppKeys.todos)) ?? []);
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (!loaded) return;
+    localStorage.setItem(TodoAppKeys.filter, selecteFilter);
+  }, [loaded, selecteFilter]);
+
+  useEffect(() => {
+    if (!loaded) return;
+    localStorage.setItem(TodoAppKeys.todos, JSON.stringify(todos));
+  }, [loaded, todos]);
 
   const handleFilterChange = (filterType) => setFilter(filterType);
   const handleAdd = (title) => {
